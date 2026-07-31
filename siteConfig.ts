44e1521f49fa2354f6184296f4304c8256a9400a
heiehiehi <1367335266@qq.com>
2026-07-31 13:09:06 +0800

    更改头像

--- siteConfig.ts
+++ siteConfig.ts
@@ -3,8 +3,8 @@
 export const siteConfig = {
   // 1. 网站标题与博主信息
   title: "代价位十辆大铲车 の 宝藏之地",
-  faviconUrl: "https://bu.dusays.com/2026/03/24/69c1e38ac1846.jpg",
-  authorName: "XingHuiSama",
+  faviconUrl: "/avatar.jpg",
+  authorName: "代价位十辆大铲车",
   bio: "在代码、学术与分子动力学模拟间穿梭的普通人。近期正埋头于 GROMACS 模拟研究与神经网络计算。",
 
   navTitle: "XingHuiSama",
@@ -15,7 +15,7 @@ export const siteConfig = {
   navAfter: "宝藏之地",
 
   // 2. 头像设置 (支持网络链接，或将图片放入 public 文件夹后使用 "/me.jpg")
-  avatarUrl: "https://bu.dusays.com/2026/03/24/69c1e38ac1846.jpg",
+  avatarUrl: "/avatar.png",
 
   // 3. 网站背景设置 (二选一)
   // 如果想用纯图片背景，请在下面 bgImage 写路径，并将 useGradient 设为 false
